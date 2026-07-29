c10836906d7eab993da13853c9dea592a06bb384
trip-pilot <chzh18-create@users.noreply.github.com>
2026-07-29 09:43:32 +0900

    deploy: singapore-share (2026-07-29 09:43)

--- singapore-share/sw.js
+++ singapore-share/sw.js
@@ -1,5 +1,5 @@
 /* trip-pilot service worker - build_site.py 가 생성 (직접 수정 금지) */
-const CACHE_NAME = "tp-singapore-share-f555f145";
+const CACHE_NAME = "tp-singapore-share-c0becf1a";
 const CACHE_RE = /^tp-singapore-share-[0-9a-f]{8}$/;
 const PRECACHE = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png", "./map-d2.png", "./map-d2-dark.png", "./map-d3.png", "./map-d3-dark.png", "./map-d4.png", "./map-d4-dark.png", "./map-d5.png", "./map-d5-dark.png", "./map-d6.png", "./map-d6-dark.png", "./thumb-d2-e3.png", "./thumb-d2-e3-dark.png", "./thumb-d2-e4.png", "./thumb-d2-e4-dark.png", "./thumb-d2-e6.png", "./thumb-d2-e6-dark.png", "./thumb-d2-e7.png", "./thumb-d2-e7-dark.png", "./thumb-d2-e8.png", "./thumb-d2-e8-dark.png", "./thumb-d2-e9.png", "./thumb-d2-e9-dark.png", "./thumb-d2-e10.png", "./thumb-d2-e10-dark.png", "./thumb-d3-e3.png", "./thumb-d3-e3-dark.png", "./thumb-d3-e5.png", "./thumb-d3-e5-dark.png", "./thumb-d3-e7.png", "./thumb-d3-e7-dark.png", "./thumb-d3-e11.png", "./thumb-d3-e11-dark.png", "./thumb-d4-e6.png", "./thumb-d4-e6-dark.png", "./thumb-d4-e8.png", "./thumb-d4-e8-dark.png", "./thumb-d5-e9.png", "./thumb-d5-e9-dark.png", "./thumb-d5-e3.png", "./thumb-d5-e3-dark.png", "./thumb-d5-e6.png", "./thumb-d5-e6-dark.png", "./thumb-d6-e3.png", "./thumb-d6-e3-dark.png", "./thumb-d6-e8.png", "./thumb-d6-e8-dark.png"];
 
